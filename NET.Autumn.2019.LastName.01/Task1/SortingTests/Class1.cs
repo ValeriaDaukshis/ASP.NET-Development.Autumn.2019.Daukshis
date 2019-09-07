a1698e48eb773d83f8d07fbb9bac77ffab0d0dc1
Valeria <daukshislera2000@gmail.com>
2019-09-07 16:45:03 +0300

    1 day 1 task

---
+++ NET.Autumn.2019.LastName.01/Task1/SortingTests/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Day1Task3;
+using NUnit.Framework;
+
+namespace SortingsLibraryTests
+{
+    [TestFixture]
+    public class SortingsTests
+    {
+        [Test]
+        public void QSortTest()
+        {
+            int[] actual = { 5, 4, 8, 0, 25, 64, -5 };
+            int[] expected = { -5, 0, 4, 5, 8, 25, 64 };
+            Sortings.QSort(actual);
+
+           Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void QSortNullTest()
+        {
+            Assert.Throws(typeof(ArgumentNullException),
+                ()=>Sortings.QSort(null));
+        }
+
+        [Test]
+        public void QSortZeroTest()
+        {
+            int[] actual = { };
+            Assert.Throws(typeof(ArgumentException),
+                () => Sortings.QSort(actual));
+        }
+
+        [Test]
+        public void MergeSortTest()
+        {
+            int[] actual = { 5, 4, 8, 0, 25, 64, -5 };
+            int[] expected = { -5, 0, 4, 5, 8, 25, 64 };
+            Sortings.MergeSort(actual);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public void MergeSortNullTest()
+        {
+            Assert.Throws(typeof(ArgumentNullException),
+                () => Sortings.MergeSort(null));
+        }
+
+        [Test]
+        public void MergeSortZeroTest()
+        {
+            int[] actual = { };
+            Assert.Throws(typeof(ArgumentException),
+                () => Sortings.MergeSort(actual));
+        }
+    }
+}
