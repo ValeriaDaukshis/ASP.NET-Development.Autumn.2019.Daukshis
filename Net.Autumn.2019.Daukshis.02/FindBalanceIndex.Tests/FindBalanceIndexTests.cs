490f64d418e1d9f6ec2f8a59cd63447dec3b5183
Valeria <daukshislera2000@gmail.com>
2019-09-21 17:05:09 +0300

    task 3 with NUnit tests

---
+++ Net.Autumn.2019.Daukshis.02/FindBalanceIndex.Tests/FindBalanceIndexTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FindBalanceIndexTask;
+using NUnit.Framework;
+
+namespace FindBalanceIndex.Tests
+{
+    [TestFixture]
+    public class FindBalanceIndexTests
+    {
+        [TestCase(new int[] { 0, 25, 34, 10, 0, 15 }, ExpectedResult = 2)]
+        [TestCase(new int[] { 0, 26, 34, 10, 0, 15 }, ExpectedResult = null)]
+        [TestCase(new int[] { 0, 20, 21, 50, 10, 101, 100, 1 }, ExpectedResult = 5)]
+        [TestCase(new int[] { 20, 21, 50, 10, 5, 101 }, ExpectedResult = 4)]
+        public object FindBalanceIndex_Array_IndexExpected(int[] array)
+            => ArrayExtension.FindBalanceIndex(array);
+
+        [Test]
+        public void FindBalanceIndex_NullArray_ArgumentNullException()
+        {
+            Assert.Throws(typeof(ArgumentNullException),
+                () => ArrayExtension.FindBalanceIndex(null));
+        }
+    }
+}
---
+++ Net.Autumn.2019.Daukshis.02/FindBalanceIndexTask/ArrayExtension.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindBalanceIndexTask
+{
+    public static class ArrayExtension
+    {
+        /// <summary>
+        /// Finds the index of the balance.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <returns>
+        /// Middle element, where the sum of elements in the left side equals to sum of the elements in the right side
+        /// </returns>
+        public static object FindBalanceIndex(int[] array)
+        {
+            CheckInput(array);
+            for (int i = 1; i < array.Length - 1; i++)
+            {
+                int leftSum = ElementsSum(array, 0, i - 1);
+                int rightSum = ElementsSum(array, i + 1, array.Length - 1);
+                if (leftSum == rightSum)
+                    return i;
+                if (leftSum + array[i] > rightSum + array[i])
+                    return null;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Sum of elements
+        /// </summary>
+        /// <param name="array">init array</param>
+        /// <param name="low">start position</param>
+        /// <param name="high">final position</param>
+        /// <returns>
+        /// Sum of elements from low to high positions
+        /// </returns>
+        private static int ElementsSum(int[] array, int low, int high)
+        {
+            if (low < 0 || high > array.Length - 1)
+                throw new ArgumentOutOfRangeException();
+            if (low > high)
+                throw new ArgumentException("Low index is more than high index");
+
+            int sum = 0;
+            for (int i = low; i <= high; i++)
+                sum += array[i];
+
+            return sum;
+        }
+
+        /// <summary>
+        /// Check input
+        /// </summary>
+        /// <param name="array">init array</param>
+        private static void CheckInput(int[] array)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("Array has zero length");
+            }
+        }
+    }
+}
